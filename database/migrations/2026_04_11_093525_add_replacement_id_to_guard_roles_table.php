1398e34d792ba478f4960ff1f0163fbbc0d5dff8
lindltaylor7 <riuyagami@gmail.com>
2026-04-20 09:55:22 -0500

    fix migrations

--- database/migrations/2026_04_11_093525_add_replacement_id_to_guard_roles_table.php
+++ database/migrations/2026_04_11_093525_add_replacement_id_to_guard_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('guard_roles', function (Blueprint $table) {
             $table->unsignedBigInteger('replacement_id')->nullable()->after('staff_id');
-            $table->foreign('replacement_id')->references('id')->on('staffs')->onDelete('set null');
+            $table->foreign('replacement_id')->references('id')->on('staff')->onDelete('set null');
         });
     }
 
