67282cb74396cc4ed09c56d8d4a2ca2241e83276
lindltaylor7 <riuyagami@gmail.com>
2026-06-17 09:37:40 -0500

    fix login nginx error

--- echo.js
+++ echo.js
@@ -7,5 +7,13 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
+    forceTLS: true,
+    unavailableTimeout: 10000,
+});
+
+window.Echo.connector.pusher.connection.bind('error', function(err) {
+    if (err && err.error && err.error.data && err.error.data.code === 403) {
+        console.log('Acceso denegado al canal privado (403). Deteniendo Echo...');
+        window.Echo.disconnect();
+    }
 });
